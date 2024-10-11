2a1ae4c5a38dac00a8609ef9f298fe5996dd6617
FZoltan40 <farkasz@kkszki.hu>
2024-10-11 12:59:07 +0200

    Feltolt method is ready.

--- Program.cs
+++ Program.cs
@@ -8,8 +8,20 @@ namespace Feladat1
 {
     internal class Program
     {
+        public static List<int> lista = new List<int>();
+        public static void feltolt()
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++) 
+            { 
+                lista.Add(rnd.Next(1,101));
+
+            }
+        }
         static void Main(string[] args)
         {
+            feltolt();
         }
     }
 }
